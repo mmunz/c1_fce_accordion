daf3e681739f39aeba124572a56d2c6bd9251ffd
Manuel Munz <munz@comuno.net>
2016-01-27 14:32:55 +0100

    fix typo

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
-T\YPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'C1 Accordion');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'C1 Accordion');
 FluidTYPO3\Flux\Core::registerProviderExtensionKey('C1.C1FceAccordion', 'Content');
 
 ?>
